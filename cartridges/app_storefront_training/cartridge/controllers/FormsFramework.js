a2f516fdce412137cf7487e5b3f7ac540e0f9f9b
Felipe Vash <felipe.silva@atonit.com.br>
2021-10-25 13:27:29 -0300

    Base files created

---
+++ cartridges/app_storefront_training/cartridge/controllers/FormsFramework.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var server = require('server');
+
+var csrfProtection = require('*/cartridge/scripts/middleware/csrf');
+var consentTracking = require('*/cartridge/scripts/middleware/consentTracking');
+
+server.get(
+    'Show',
+    server.middleware.https,
+    consentTracking.consent,
+    csrfProtection.generateToken,
+    function (req, res, next) {
+        var reportingURLs;
+
+        res.setViewData({ reportingURLs: reportingURLs });
+
+        res.render('forms_framework/forms_freamwork');
+        next();
+    }
+);

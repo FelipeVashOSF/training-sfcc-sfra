2302b3d4828bcf8ec5925082d8c2ff8055da732d
Felipe Vash <felipe.silva@atonit.com.br>
2021-11-17 06:32:59 -0300

    Requested Fixes

--- cartridges/app_storefront_training/cartridge/client/default/js/main.js
+++ cartridges/app_storefront_training/cartridge/client/default/js/main.js
@@ -1,7 +1,8 @@
 window.jQuery = window.$ = require('jquery');
 var processInclude = require('./util');
+var cart = require('./cart');
 
 $(document).ready(function () {
-    processInclude(require('./cart'));
+    processInclude(cart);
     processInclude(require('../../../../../app_storefront_base/cartridge/client/default/js/main'));
 });
